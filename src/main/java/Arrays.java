685e36db99d366184e0795cec02e48e39d05299c
Lenuara <keypost777@gmail.com>
2022-06-22 17:06:48 +0300

    Немного работы с массивами

--- src/main/java/Arrays.java
+++ src/main/java/Arrays.java
@@ -12,22 +12,46 @@ public class Arrays {
         String g = greeting.toLowerCase();
         System.out.println(g);
         Object[] array3 = {"vPCkuac", 1, 15.0};
-        //String parseStr;
-        System.out.println();
-        int[] Arr = {1, 8, 14, 43, 16, 37, 102, 1991};
+
+        System.out.println("===============");
+
+        //Найдем количество нечетных элементов массива и максимальное значение нечетного числа
+        int[] Arr = {1, 8, 14, 43, 16, 37, 102, 1991, 1};
+        // напечатаем весь массив
         int count = 0;
         int max = 0;
         for (int x : Arr) {
-            if (x % 2 != 0) {
+            System.out.print(x+ " ");
+                if (x % 2 != 0) {
                 count +=1;
                 if(x> max){
                     max =x;
                 }
-
             }
-
         }
-        System.out.println(count);
-        System.out.println(max);
+        System.out.println("");
+
+        //найдем сумму всех элементов массива Arr c использованием длины
+        int sum = 0;
+        for (int i = 0; i<Arr.length; i++){
+            sum +=Arr[i];
+        }
+
+        System.out.println("кол-во нечетных эл-тов = " + count);
+        System.out.println("max = " + max);
+        System.out.println("сумма всех эл-тов = " + sum);
+        System.out.println("===============");
+
+        // развернем массив символов наоборот
+        String str = "I learning Java!";
+        char[] array_chars = str.toCharArray();
+        String result = "";
+       for (int x = array_chars.length-1; x>=0; x--){
+            result = result + array_chars[x];
+        }
+        System.out.println("Исходная строка = " + str);
+        System.out.println("Строка наоборот = " + result);
+        System.out.println("===============");
+
     }
 }
